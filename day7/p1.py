d4eb05337fbfb436cc96f783160e1bddfc11dca3
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-12-08 13:56:49 -0500

    changed the node dictionary keys to the path

--- day7/p1.py
+++ day7/p1.py
@@ -1,9 +1,14 @@
 from anytree import Node, RenderTree, search, Walker
 
-with open('day7/test.in', 'r', encoding="utf-8") as f:
+def getPath(cdStack):
+  return '/'.join(cdStack)
+
+
+with open('day7/input.in', 'r', encoding="utf-8") as f:
   commands = f.read().split('\n')
   cdStack = [] # tracks the current cd and its path
-  root = Node('', None, type='dir', size=0)
+  root = Node('~', None, type='dir', size=0)
+
   nodes = {'/': root}
   sumToDelete = 0
   w = Walker()
@@ -21,10 +26,12 @@ with open('day7/test.in', 'r', encoding="utf-8") as f:
     # make new directory
     elif line.startswith('dir'):
       dirName = line[4:]
-      # add a node to the dictionary with the dir name as the key and the Node
+      # add a node to the dictionary with the dir path as the key and the Node
       # object as the value, and the top of the cdStack as the parent
-      nodes[dirName] = Node(dirName, nodes[cdStack[-1]], type='dir', size=0)
-      # print(f'new dir: {nodes[dirName]}')
+      parentPath = getPath(cdStack)
+      dpath = parentPath + '/' + dirName
+      nodes[dpath] = Node(dirName, nodes[parentPath], type='dir', size=0)
+      # print(f'new dir: {nodes[dpath]}')
 
     # ignore
     elif line.startswith('$ ls'):
@@ -33,17 +40,34 @@ with open('day7/test.in', 'r', encoding="utf-8") as f:
     # add new file to directory
     else:
       fsize, fname = line.split(' ')
-      nodes[fname] = Node(fname, nodes[cdStack[-1]], type='file', size=int(fsize))
+      parentPath = getPath(cdStack)
+      fpath = parentPath + '/' + fname
+      nodes[fpath] = Node(fname, nodes[parentPath], type='file', size=int(fsize))
 
+
+  
+  sumToDelete2 = float('inf')
   
   # calculate the size of each dir
   for dir in search.findall(root, filter_=lambda node: node.type == 'dir'):
     files = search.findall(dir, filter_=lambda node: node.type == 'file')
-    print(f'chilren of {dir.name}: {files}')
+    # print(f'chilren of {dir.name}: {files}')
     dir.size = sum(int(f.size) for f in files)
     if(dir.size <= 100000):
       sumToDelete += dir.size
+      
+    TOTAL_DISK_SPACE = 70000000
+    SPACE_NEEDED = 30000000
+    SPACE_USED = nodes['/'].size
+    SPACE_AVAILABLE = TOTAL_DISK_SPACE - SPACE_USED
+    SPACE_TO_FREE = SPACE_NEEDED - SPACE_AVAILABLE
+    print (SPACE_NEEDED, SPACE_AVAILABLE, SPACE_TO_FREE)
+    if(dir.size < sumToDelete2 and dir.size > SPACE_TO_FREE):
+      sumToDelete2 = dir.size
     
-  print(RenderTree(root))
+    
 
-  print(f'sumToDelete: {sumToDelete}')
+  # print(RenderTree(root))
+
+  print(f'[PART 1]: sumToDelete: {sumToDelete}')
+  print(f'[PART 2]: sumToDelete: {sumToDelete2}')
