916858d8619743d65e4cffd8a4c544aad916b12d
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-12-10 13:40:12 -0500

    pepeW

--- day9/p1.py
+++ day9/p1.py
@@ -4,24 +4,71 @@ tailSet = {(0,0)}
 
 def move(dir, dist):
   global posHead
-  p = list(posHead)
+  # p = list(posHead)
   for i in range(dist):
     if(dir == 'R'):
       # move the x coord in the positive direction
-      p[0] += 1
+      posHead = (posHead[0] + 1, posHead[1])
     elif(dir == 'L'):
       # move the x coord in the negative direction
-      p[0] -= 1
+      posHead = (posHead[0] - 1, posHead[1])
     elif(dir == 'U'):
       # move the y coord in the positive direction
-      p[1] += 1
+      posHead = (posHead[0], posHead[1] + 1)
     elif(dir == 'D'):
       # move the y coord in the negative direction
-      p[1] -= 1
-    posHead = tuple(p)  
+      posHead = (posHead[0], posHead[1] - 1)
+    checkTail()
     headQueue.append(posHead)
       
-    
+def checkTail():
+  global posTail
+  headX, headY = posHead[0], posHead[1]
+  tailX, tailY = posTail[0], posTail[1]
+  if abs(headX - tailX) <= 1 and abs(headY - tailY) <= 1:
+    return
+  
+  # move Y axis
+  if headX == tailX:
+    # tail under head
+    if headY - tailY > 0:
+      posTail = (tailX, tailY + 1)
+    # tail over head
+    if headY - tailY < 0:
+      posTail = (tailX, tailY - 1)
+  
+  # move X axis
+  elif headY == tailY:
+    # tail left of head
+    if headX - tailX > 0:
+      posTail = (tailX + 1, tailY)
+    # tail right of head
+    if headY == tailY and headX - tailX < 0:
+      posTail = (tailX - 1, tailY)
+
+  else:
+    # X is too far
+    if headX - tailX >= 2 and headY - tailY > 0:
+      posTail = (tailX + 1, tailY + 1)
+    elif headX - tailX >= 2 and headY - tailY < 0:
+      posTail = (tailX + 1, tailY - 1)
+    elif headX - tailX <= -2 and headY - tailY > 0:
+      posTail = (tailX - 1, tailY + 1)
+    elif headX - tailX <= -2 and headY - tailY < 0:
+      posTail = (tailX - 1, tailY - 1)
+
+    # Y is too far
+    elif headY - tailY >= 2 and headX - tailX > 0:
+      posTail = (tailX + 1, tailY + 1)
+    elif headY - tailY >= 2 and headX - tailX < 0:
+      posTail = (tailX - 1, tailY + 1)
+    elif headY - tailY <= -2 and headX - tailX > 0:
+      posTail = (tailX + 1, tailY - 1)
+    elif headY - tailY <= -2 and headX - tailX < 0:
+      posTail = (tailX - 1, tailY - 1)
+  # print(f'tail move to {posTail}')
+  tailSet.add(posTail)
+
 
 def tooFar():
   headX, headY = posHead[0], posHead[1]
@@ -33,12 +80,14 @@ def tooFar():
   return abs(headX - tailX) + abs(headY - tailY) > 2 
 
 
-with open('day9/test.in', 'r', encoding="utf-8") as f:
+with open('day9/input.in', 'r', encoding="utf-8") as f:
   # read data
   lines = f.read().split('\n')
 
   for line in lines:
     direction, distance = line.split(' ')
     move(direction, int(distance))
-  print(headQueue)
+  # print(headQueue)
+  # print(tailSet)
+  print(f'[PART 1] Number of spaces visited: {len(tailSet)}')
     
