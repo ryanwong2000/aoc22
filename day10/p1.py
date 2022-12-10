acb27458aea460f9eb8860385d8a48c840cbe3ef
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-12-10 16:30:32 -0500

    make change

--- day10/p1.py
+++ day10/p1.py
@@ -2,7 +2,7 @@ with open('day10/input.in', 'r', encoding="utf-8") as f:
   lines = f.read().split('\n')
 
   Xregister = 1
-  count = 1
+  count = 0
   Vqueue = [0 * 3]
   cycles = [(count, Xregister)]
   totalscore = 0
@@ -16,13 +16,16 @@ with open('day10/input.in', 'r', encoding="utf-8") as f:
       addx, V = line.split()
       count += 1
       cycles.append((count, Xregister))
-      Xregister += int(V)
       count += 1
       cycles.append((count, Xregister))
-  # print(cycles)
+      Xregister += int(V)
+
   for x in range(20, count, 40):
-    score = cycles[x-1][0] * cycles[x-1][1]
+    score = cycles[x][0] * cycles[x][1]
     totalscore += score
-    print(cycles[x-1], score)
+    print(cycles[x], score)
+  
+  
+
   print(f'[PART 1] Total score: {totalscore}')
   
