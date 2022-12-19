9ce2ffe6b18fe26a3564d35eb5310fc71f03a475
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-12-18 19:39:52 -0500

    birthday advent yay

--- day12/p1.py
+++ day12/p1.py
@@ -1,8 +1,81 @@
-import numpy as np
-with open('day12/test.in', 'r', encoding="utf-8") as f:
+# import numpy as np
+
+def canDescend(current, dest):
+  if current == 'S':
+    current = 'a' # S shares the lowest elevation as a
+  if dest == 'E':
+    dest = 'z' # E shares the highest elevation as z
+  # print(ord(current), current, dest)
+  return ord(current) >= ord(dest)-1
+
+with open('day12/input.in', 'r', encoding="utf-8") as f:
   data = f.read().split('\n')
-  print(data)
+  # print(data)
   matrix = [list(line) for line in data]
-  # matrix = np.asmatrix(matrix)
-  x = np.where(matrix == 'S')
-  print(x)
+  # print(matrix)
+
+  numRows = len(matrix)
+  numCols = len(matrix[0])
+
+
+  # find the Start and End coords as (row, col)
+  for iLine, line in enumerate(matrix):
+    for iEl, el in enumerate(line):
+      if el == 'S':
+        startCoords = (iLine, iEl)
+      if el == 'E':
+        endCoords = (iLine, iEl)
+  # print(startCoords, endCoords)
+
+  # matrix to track coords we visit
+  visited = [[ False for _ in range(numCols) ] for _ in range(numRows)]
+  visited[startCoords[0]][startCoords[1]] = True # set starting coordinates to True
+  # queue to track the cells that need to be explored
+  toExplore = [startCoords] # start with starting coords
+
+  nextLayerCount = 0 # (havent found the next layer yet)
+  leftInLayer = 1 # (the starting cell)
+  moveCount = 0
+
+  # define direction vectors
+  #      N  E  W  S
+  dr = [-1, 0, 0, 1]
+  dc = [0, 1, -1, 0]
+
+  # keep finding the adjacents until we run out of cell to visit
+  while toExplore:
+    currentCoords = toExplore.pop(0)
+    # print(currentCoords)
+    # check if we reached the end
+    if currentCoords == endCoords:
+      break
+    currentR, currentC = currentCoords
+    # -- GET ADJACENT CELLS --
+    # go through each dir vector
+    for i in range(4):
+      rr = currentR + dr[i]
+      cc = currentC + dc[i]
+
+      # check out of bounds
+      if rr < 0 or cc < 0 or rr > numRows-1 or cc > numCols-1:
+        continue
+      # check if already visited
+      if visited[rr][cc]:
+        continue
+      # check if the next one is higher
+      # print('current', currentR, currentC)
+      if not canDescend(matrix[currentR][currentC], matrix[rr][cc]):
+        continue
+      # add to queue
+      toExplore.append((rr, cc))
+      visited[rr][cc] = True
+      nextLayerCount += 1
+    
+    leftInLayer -= 1
+    if leftInLayer == 0:
+      leftInLayer = nextLayerCount
+      nextLayerCount = 0
+      moveCount += 1
+      # print(moveCount, matrix[currentR][currentC])
+  
+  print(f'[PART 1] Least number fo moves to reach E: {moveCount}')
